3fde5060d69deb2fc4f5535720d3886fd8619f53
Mubashir Hussain <124498517+MubashirJatoi@users.noreply.github.com>
2024-11-28 18:38:46 +0500

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,7 +4,7 @@ import Logo from "../../public/Images/logo-removebg-preview.png"
 
 function Navbar() {
     return(
-        <div className="navbar items-center flex justify-between bg-[#121212] h-[80px] fixed top-0 w-[100%] z-[100]">
+        <nav className="navbar items-center flex justify-between bg-[#121212] h-[80px] fixed top-0 w-[100%] z-[100]">
             <Link href={"/"}><Image className="logo ml-[20px] " src={Logo} alt="Logo" width={100} height={20}/></Link>
             <div className="links-parent flex justify-center gap-[60px] text-[20px] bg-transparent">
                 <Link className="links no-underline decoration-[none] text-[#959595] bg-transparent hover:text-[#FF6F00]" href={"/"}>Home</Link>
@@ -14,7 +14,7 @@ function Navbar() {
                 <Link className="links no-underline decoration-[none] text-[#959595] bg-transparent hover:text-[#FF6F00]" href={"/Contactme"}>Contact me</Link>
             </div>
             <Link className="phonu bg-transparent flex no-underline decoration-[none]" href="tel:+923701135722"><button className="hire-me text-[#FFFFFF] bg-[#FD6F00] rounded-[8px] w-[150px] h-[50px] border-[#FD6F00] mr-[50px] text-[16px] cursor-pointer hover:mt-[5px] hover:transition-colors hover:duration-100 hover:ease-in-out hover:[color-transition:1s]">Hire Me</button></Link>
-        </div>
+        </nav>
     );
 };
 
